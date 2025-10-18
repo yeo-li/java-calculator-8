80a0e0e5cfbbb3610fe0ae465c5c5dcf07f34129
yeo-li <parkseongyeol2110@gmail.com>
2025-10-19 00:15:45 +0900

    test(CalculatorServiceTest): parseCustomDelimiter() 메서드 테스트 코드 작성

---
+++ src/test/java/calculator/sevice/CalculatorServiceTest.java
@@ -0,0 +1,108 @@
+package calculator.sevice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CalculatorServiceTest {
+
+    private final CalculatorService calculatorService = new CalculatorService();
+
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    "//d\\n1d2d3, d",
+                    "//park\\n1park2park3, park",
+                    "//\\\\n1\\2\\3, \\",
+                    "//\\n1\\2\\3, ''",
+                    "1:2:3, ''"
+            }
+    )
+    @DisplayName("사용자 입력값으로부터 커스텀 구분자 추출 - 성공")
+    void parseCustomDelimiterSuccess(String input, String expectedDelimiter) {
+        // when
+        String customDelimiter = calculatorService.parseCustomDelimiter(input);
+
+        // then
+        Assertions.assertEquals(expectedDelimiter, customDelimiter);
+    }
+
+    @Test
+    @DisplayName("사용자 입력값으로부터 커스텀 구분자 추출 - \"//\"만 존재하는 경우")
+    void parseCustomDelimiterCustomDelimiterInvalid() {
+        // given
+        String input = "//d1d2d3";
+
+        // when & then
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    calculatorService.parseCustomDelimiter(input);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("사용자 입력값으로부터 커스텀 구분자 추출 - \\n만 존재하는 경우")
+    void parseCustomDelimiterCustomDelimiterInvalid2() {
+        // given
+        String input = "\\nd1d2d3";
+
+        // when & then
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    calculatorService.parseCustomDelimiter(input);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("사용자 입력값으로부터 커스텀 구분자 추출 - \"\\n\"이 \"//\" 보다 앞에 있는 경우")
+    void parseCustomDelimiterCustomDelimiterInvalid3() {
+        // given
+        String input = "\\nd//3d1d2d3";
+
+        // when & then
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    calculatorService.parseCustomDelimiter(input);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("사용자 입력값으로부터 커스텀 구분자 추출 - 커스텀 구분자 등록 문법이 맨 앞에 있지 않는 경우")
+    void parseCustomDelimiterCustomDelimiterInvalid4() {
+        // given
+        String input = "3d1d2d3//d\\n";
+
+        // when & then
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    calculatorService.parseCustomDelimiter(input);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("사용자 입력값으로부터 커스텀 구분자 추출 - 커스텀 구분자가 숫자인 경우")
+    void parseCustomDelimiterCustomDelimiterIsNumber() {
+        // given
+        String input = "//1\\n1d2d3";
+
+        // when & then
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    calculatorService.parseCustomDelimiter(input);
+                }
+        );
+    }
+
+
+}
