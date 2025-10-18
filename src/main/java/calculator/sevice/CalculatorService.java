3e6ffb1bb4340a6c0cde500016ddaab0aa2f86cd
yeo-li <parkseongyeol2110@gmail.com>
2025-10-18 22:21:57 +0900

    feat(CalculatorService): extractCalculationBody() 및 parseNumbers() 메서드 구현

--- src/main/java/calculator/sevice/CalculatorService.java
+++ src/main/java/calculator/sevice/CalculatorService.java
@@ -1,12 +1,15 @@
 package calculator.sevice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculatorService {
 
-    public String extractCustomDelimiter(String input) {
+    public String parseCustomDelimiter(String input) {
         validateCustomDelimiter(input);
 
         if (!hasCustomDelimiter(input)) {
-            return ""; // todo: constant
+            return "";
         }
 
         int start = input.indexOf("//") + 2;
@@ -14,6 +17,52 @@ public class CalculatorService {
 
         return input.substring(start, end);
     }
+
+    public String extractCalculationBody(String input) {
+        validateCustomDelimiter(input);
+
+        String numberSection = input;
+        if (hasCustomDelimiter(input)) {
+            int start = input.indexOf("\\n") + 3;
+            numberSection = input.substring(start);
+        }
+
+        return numberSection;
+    }
+
+    public List<Integer> parseNumbers(String calculationBody, String customDelimiter) {
+        String regex = ",|:|" + customDelimiter;
+
+        String[] parsedCalculationBody = calculationBody.split(regex);
+        validateCalculationBody(parsedCalculationBody);
+
+        List<Integer> numbers = new ArrayList<>();
+
+        // converter
+        for (String number : parsedCalculationBody) {
+            numbers.add(Integer.parseInt(number));
+        }
+
+        return numbers;
+    }
+
+    private void validateCalculationBody(String[] calculationBody) {
+        for (String number : calculationBody) {
+            if (!isNumber(number)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private boolean isNumber(String number) {
+        for (char c : number.toCharArray()) {
+            if ('0' > c || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     private boolean hasCustomDelimiter(String input) {
         return input.contains("//") && input.contains("\\n");
